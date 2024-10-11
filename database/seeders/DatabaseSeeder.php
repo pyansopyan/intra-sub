b4ddd0e5fbe3f6322b82dd10f8c9fb56f08342ac
kytonas <faikarksatria@gmail.com>
2024-10-11 10:39:46 +0700

    data seeder admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin Intra-sub',
+            'email' => 'adminintrasub@gmail.com',
+            'password' => bcrypt('adminintrasub')
+        ]); 
     }
 }
