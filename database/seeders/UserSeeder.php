ebb5bc8525056e17988ea521f6b8933af45d63b0
muhamadazis32 <muhamadazis9a@gmail.com>
2024-10-15 08:48:21 +0700

    feat:  Add userSeeder penerapan role & permission

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Masukin permission baru disini
+        $collection = collect([
+            'User',
+            'Role',
+            'Permission',
+        ]);
+
+        $subPermission = collect([
+            'view',
+            'create',
+            'edit',
+            'delete',
+        ]);
+
+        $collection->each(function ($item, $key) use ($subPermission) {
+            Permission::updateOrCreate(
+                ['name' => 'manage' . $item],
+                ['guard_name' => 'web', 'name' => 'manage' . $item]
+            );
+            $subPermission->each(function ($subItem, $subKey) use ($item) {
+                Permission::updateOrCreate(
+                    ['name' => 'manage' . $item . '-' . $subItem],
+                    ['guard_name' => 'web', 'name' => 'manage' . $item . '-' . $subItem]
+                );
+            });
+        });
+
+        // Membuat atau memperbarui pengguna admin
+        $userAdmin = User::updateOrCreate(
+            ['email' => 'admin@gmail.com'], // Unique identifier
+            [
+                'name' => 'Administrator',
+                'password' => bcrypt('password'), // Hanya update password jika perlu
+                'nrp' => '1234',
+            ]
+        );
+
+        // Membuat atau memperbarui role superadmin
+        $roleAdmin = Role::updateOrCreate(['name' => 'superadmin']);
+        $roleAdmin->givePermissionTo(Permission::all()); // Memberikan semua permission kepada superadmin
+        $userAdmin->assignRole($roleAdmin);
+
+        // Membuat atau memperbarui pengguna staff
+        $userTeacher = User::updateOrCreate(
+            ['email' => 'staff@gmail.com'], // Unique identifier
+            [
+                'name' => 'Staff',
+                'password' => bcrypt('password'), // Hanya update password jika perlu
+                'nrp' => '123',
+            ]
+        );
+
+
+        // Membuat atau memperbarui role staff
+        $roleTeacher = Role::updateOrCreate(['name' => 'staff']);
+
+        // Memberikan permission tertentu untuk staff
+        $roleTeacher->givePermissionTo(Permission::whereIn('id', ['16', '17'])->get());
+        $userTeacher->assignRole($roleTeacher);
+    }
+}
