175bc9a6c575b54e9ace86c56f831dc504cde4a0
Cndynajae <csagitasari@gmail.com>
2024-10-18 09:12:54 +0700

    mengganti bug

--- resources/views/livewire/admin/sidebar.blade.php
+++ resources/views/livewire/admin/sidebar.blade.php
@@ -7,7 +7,7 @@
             <a href="/user">User</a>
             <ul class="mt-6">
                 <li class=" px-6 py-3">
-                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                    <span class="absolute inset-y-0 left-0 w-1 bg-white-600 rounded-tr-lg rounded-br-lg"
                         aria-hidden="true"></span>
                     <a href="/" wire:navigate
                         class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100">
