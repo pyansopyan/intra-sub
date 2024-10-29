4cb0cd65080d3f1e2751b31697bd2b6e1b48a18e
Cndynajae <csagitasari@gmail.com>
2024-10-29 10:55:18 +0700

    rubah tempat change password

--- resources/views/livewire/admin/header.blade.php
+++ resources/views/livewire/admin/header.blade.php
@@ -52,6 +52,13 @@
                                     <span>Edit Profile</span>
                                 </a>
                             </li>
+                             <li class="flex">
+                                <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                                    href="{{route('change.password')}}">
+                                    <i class='bx bxs-lock' style="font-size: 16px;"></i>
+                                    <span class="ml-2">Change Password</span>
+                                </a>
+                            </li>
                             <form action="{{ route('logout') }}" method="POST">
                                 @csrf
                                 <button
