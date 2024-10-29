fb2ddaeb160ad66fa4aae25a5c319fdbc231d205
kytonas <faikarksatria@gmail.com>
2024-10-29 10:42:51 +0700

    fix: merubah icon sidebar menggunakan boxicons

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
 </head>
 
 <body>
--- resources/views/livewire/admin/sidebar.blade.php
+++ resources/views/livewire/admin/sidebar.blade.php
@@ -11,12 +11,7 @@
                         aria-hidden="true"></span>
                     <a href="/" wire:navigate
                         class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-                            </path>
-                        </svg>
+                        <i class="bx bxs-home" style="font-size: 20px;"></i>
                         <span class="ml-4">Dashboard</span>
                     </a>
                 </li>
@@ -26,9 +21,7 @@
                     <li class="relative px-6 py-3 {{ request()->is('user*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 "
                             href="{{ route('user.index') }}" wire:navigate>
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M12 12c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm0 2c-3.31 0-8 1.67-8 5v1h16v-1c0-3.33-4.69-5-8-5z"/>
-                            </svg>
+                            <i class='bx bxs-user' style="font-size: 20px;"></i>
                             <span class="ml-4">User</span>
                         </a>
                     </li>
@@ -39,9 +32,7 @@
                     <li class="relative px-6 py-3 {{ request()->is('role*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="{{ route('role.index') }}" wire:navigate>
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M12 2C7.03 2 2 3.5 2 7v6c0 3.5 5.03 5 10 5s10-1.5 10-5V7c0-3.5-5.03-5-10-5zm0 16c-2.84 0-5.64-.62-7.5-1.74v-5.26c1.86 1.12 4.66 1.74 7.5 1.74s5.64-.62 7.5-1.74v5.26C17.64 17.38 14.84 18 12 18z"/>
-                            </svg>
+                            <i class='bx bxs-user-check' style="font-size: 24px;"></i>
                             <span class="ml-4">Role</span>
                         </a>
                     </li>
@@ -53,9 +44,7 @@
                     <li class="relative px-6 py-3 {{ request()->is('permissions*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="{{ route('permission.index') }}" wire:navigate>
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M12 2a5 5 0 0 1 5 5v3h-1a5 5 0 1 0-10 0v5h12v2H6a2 2 0 0 1-2-2V9a5 5 0 0 1 5-5zm5 13h2v4h-2z"/>
-                            </svg>
+                            <i class='bx bxs-check-square' style="font-size: 20px;"></i>
                             <span class="ml-4">Permission</span>
                         </a>
                     </li>
@@ -67,9 +56,7 @@
                     <li class="relative px-6 py-3 {{ request()->is('departement*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="{{ route('departement.index') }}" wire:navigate>
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M4 3h16v18H4V3zm3 2v14h2V5H7zm8 0v14h2V5h-2zm-6 6v8h2v-8H9zm4 0v8h2v-8h-2z" />
-                            </svg>
+                           <i class='bx bxs-user-badge' style="font-size: 21px;"></i>
                             <span class="ml-4">Departement</span>
                         </a>
                     </li>
@@ -81,11 +68,7 @@
                     <li class="relative px-6 py-3 {{ request()->is('bagian*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="{{ route('bagian.index') }}" wire:navigate>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                <path d="M16 12H8M16 16H8M16 8H8" />
-                                <path d="M4 6V18c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V6H4z" />
-                            </svg>
+                            <i class='bx bxs-user-pin' style="font-size: 20px;"></i>
                             <span class="ml-4">Bagian</span>
                         </a>
                     </li>
@@ -98,10 +81,7 @@
                     <li class="relative px-6 py-3 {{ request()->is('jabatan*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="{{ route('jabatan.index') }}" wire:navigate>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                <path d="M12 2C10.34 2 9 3.34 9 5c0 1.66 1.34 3 3 3s3-1.34 3-3c0-1.66-1.34-3-3-3zm-1 9c-5.52 0-10 3.58-10 8v1h20v-1c0-4.42-4.48-8-10-8z" />
-                            </svg>
+                           <i class='bx bxs-user-rectangle' style="font-size: 20px;"></i>
                             <span class="ml-4">Jabatan</span>
                         </a>
                     </li>
@@ -113,11 +93,7 @@
                     <li class="relative px-6 py-3 {{ request()->is('task-status*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="{{ route('task-status.index') }}" wire:navigate>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                <path d="M12 2L2 7h20L12 2zm0 4l10 5-10 5L2 11l10-5z" />
-                                <path d="M2 17h20M2 22h20" />
-                            </svg>
+                            <i class='bx bxs-check-circle' style="font-size: 20px;"></i>
                             <span class="ml-4">Task Status</span>
                         </a>
                     </li>
@@ -155,7 +131,7 @@
             </ul>
             <ul>
                 @can('manageTaskPriorities')
-                    <li class="relative px-6 py-3">
+                    <li class="relative px-6 py-3 {{ request()->is('priorities*') ? 'text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700' : 'text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200' }}"">
                         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                             href="{{ route('priorities.index') }}" wire:navigate>
                             <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
