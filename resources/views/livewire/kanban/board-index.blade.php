ae38a48353b9692d26488ba731fa741437239bbc
Cndynajae <csagitasari@gmail.com>
2024-11-14 13:03:13 +0700

    fix: sedikit rubah pada board

--- resources/views/livewire/kanban/board-index.blade.php
+++ resources/views/livewire/kanban/board-index.blade.php
@@ -2,14 +2,14 @@
     <div class="breadcrumbs text-sm mt-4">
         <ul>
             <li><a href="{{ route('welcome') }}">Dashboard</a></li>
-            <li><a href="{{ route('board.index') }}" class="text-gray-400 font-semibold">Board</a></li>
+            <li><a href="{{ route('board.index') }}" class="text-black-400 font-semibold">Board</a></li>
         </ul>
     </div>
     <a href="{{ route('welcome') }}" class="btn btn-md bg-white text-black mt-2">
         <i class="bx bx-arrow-back text-xl"></i>
     </a>
     <h2 class="text-2xl font-bold text-gray-800 mb-2 dark:text-gray-200 mt-4 text-black">Board</h2>
-    <p class="text-gray-600 mb-4 dark:text-gray-400">In this section you can choose one of your projects to show its Scrum or Kanban board</p>
+    
 
     <div class="flex justify-center mb-4">
         <div class="p-6 rounded-lg shadow-md max-w-2xl dark:bg-gray-800 bg-white w-full">
