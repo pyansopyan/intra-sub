c695f4374af01c70cddd5cfa0c3551c680be9151
Cndynajae <csagitasari@gmail.com>
2024-11-14 14:10:53 +0700

    perbaiki tampilan kanban

--- resources/views/livewire/kanban/board-index.blade.php
+++ resources/views/livewire/kanban/board-index.blade.php
@@ -9,22 +9,22 @@
         <i class="bx bx-arrow-back text-xl"></i>
     </a>
     <h2 class="text-2xl font-bold text-gray-800 mb-2 dark:text-gray-200 mt-4 text-black">Board</h2>
-    
+
 
     <div class="flex justify-center mb-4">
         <div class="p-6 rounded-lg shadow-md max-w-2xl dark:bg-gray-800 bg-white w-full">
             <label for="project" class="block text-sm font-medium text-gray-700 mb-1 dark:text-gray-200 text-black">
-                Project <span class="text-red-500">*</span>
+                Proyek <span class="text-red-500">*</span>
             </label>
-            <select wire:model.live="selectedProject" id="project" class="w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 bg-white rounded-lg focus:ring-purple-500 focus:border-purple-500">
+            <select wire:model.live="selectedProject" id="project" class="w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-black-300 bg-white rounded-lg focus:ring-purple-500 focus:border-purple-500">
                 @can('manageKanban-view')
-                <option value="" class="text-gray-400 dark:text-gray-400">Select an option</option>
+                <option value="" class="text-black-400 dark:text-black-400">Pilih Proyek</option>
                 @foreach ($projects as $project)
-                    <option value="{{ $project->id }}" class="text-gray-400 dark:text-gray-400">{{ $project->name }}</option>
+                    <option value="{{ $project->id }}" class="text-black-400 dark:text-black-400">{{ $project->name }}</option>
                 @endforeach
                 @endcan
             </select>
-            <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Choose a project to show its board</p>
+            <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Pilih proyek untuk menampilkan papan tugasnya.</p>
         </div>
     </div>
 </div>
