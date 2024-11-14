0f10d08c894def22e0f9d2a251f4ebfe71b9a6ed
kytonas <faikarksatria@gmail.com>
2024-11-14 10:32:26 +0700

    fix: remove wireUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     presets: [
-        require("./vendor/wireui/wireui/tailwind.config.js"),
         require('@tailwindcss/forms'),
     ],
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-        "./vendor/wireui/wireui/src/*.php",
-        "./vendor/wireui/wireui/ts/**/*.ts",
-        "./vendor/wireui/wireui/src/WireUi/**/*.php",
-        "./vendor/wireui/wireui/src/Components/**/*.php",
     ],
     theme: {
         extend: {
-            colors: {
-                secondary: {
-                    100: '#ffffff',
-                    200: '#ffffff',
-                    300: '#ffffff',
-                    400: '#ffffff',
-                    500: '#ffffff',
-                    600: '#ffffff',
-                    700: '#ffffff',
-                    800: '#ffffff',
-                    900: '#ffffff',
-                },
-            },
         },
     },
     plugins: [
