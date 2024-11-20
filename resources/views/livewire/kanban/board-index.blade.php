befa7769d5f83d28027109b55ea3f08137790559
Cndynajae <csagitasari@gmail.com>
2024-11-20 11:34:25 +0700

    menghapus satu tampilan di dashboard

--- resources/views/livewire/kanban/board-index.blade.php
+++ resources/views/livewire/kanban/board-index.blade.php
@@ -14,17 +14,17 @@
     <div class="flex justify-center mb-4">
         <div class="p-6 rounded-lg shadow-md max-w-2xl dark:bg-gray-800 bg-white w-full">
             <label for="project" class="block text-sm font-medium text-gray-700 mb-1 dark:text-gray-200 text-black">
-                Proyek <span class="text-red-500">*</span>
+                Project <span class="text-red-500">*</span>
             </label>
             <select wire:model.live="selectedProject" id="project" class="w-full p-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-black-300 bg-white rounded-lg focus:ring-purple-500 focus:border-purple-500">
                 @can('manageKanban-view')
-                <option value="" class="text-black-400 dark:text-black-400">Pilih Proyek</option>
+                <option value="" class="text-black-400 dark:text-black-400">Choose Project</option>
                 @foreach ($projects as $project)
                     <option value="{{ $project->id }}" class="text-black-400 dark:text-black-400">{{ $project->name }}</option>
                 @endforeach
                 @endcan
             </select>
-            <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Pilih proyek untuk menampilkan papan tugasnya.</p>
+            <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Select a project to display the board.</p>
         </div>
     </div>
 </div>
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -70,7 +70,7 @@
         <!-- Cards -->
         <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
             <!-- Card -->
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+            {{-- <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
                 <div
                     class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
@@ -87,7 +87,7 @@
                         6389
                     </p>
                 </div>
-            </div>
+            </div> --}}
         </div>
 
         <!-- New Table -->
