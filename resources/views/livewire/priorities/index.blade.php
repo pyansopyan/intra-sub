e848249ea0de1620ba5561f819d3ab85ac07fe48
Cndynajae <csagitasari@gmail.com>
2024-11-21 14:53:34 +0700

    mengubah nama di konfirmasi hapus

--- resources/views/livewire/priorities/index.blade.php
+++ resources/views/livewire/priorities/index.blade.php
@@ -126,7 +126,7 @@
                                                     stroke-linejoin="round" stroke-width="2"
                                                     d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                             </svg>
-                                            <h3 class="text-lg font-bold">Apakah anda mau menghapus type ini</h3>
+                                            <h3 class="text-lg font-bold">Apakah anda mau menghapus priority ini</h3>
                                             <div class="modal-action">
                                                 <button
                                                     class="btn bg-red-500 text-white hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700 border-none"
