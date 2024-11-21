11d0ac3045ba1ff5c3ac21b4b2ca3e9509cedd88
Cndynajae <csagitasari@gmail.com>
2024-11-21 14:37:48 +0700

    mengubah bahasa di halaman bagian

--- resources/views/livewire/bagian/edit.blade.php
+++ resources/views/livewire/bagian/edit.blade.php
@@ -16,7 +16,7 @@
     <form wire:submit.prevent="update" enctype="multipart/form-data">
         <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
             <label class="block text-sm">
-                <span class="text-gray-700 dark:text-gray-400">Nama</span>
+                <span class="text-gray-700 dark:text-gray-400">Name</span>
                 <input
                     class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input text-black"
                     placeholder="Insert bagian name" wire:model="name" />
