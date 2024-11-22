448fa2ce84d92256068ade5b52dc122912840b51
kytonas <faikarksatria@gmail.com>
2024-11-22 10:47:18 +0700

    fix: background isToday calendar di dashboard

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -69,8 +69,9 @@
                             @foreach ($week as $date)
                                 <div
                                     class="p-2 rounded-md
-                            {{ $date['isCurrentMonth'] ? 'bg-gray-100 dark:bg-gray-700' : 'bg-gray-200 dark:bg-gray-600' }}
-                            {{ $date['isToday'] ? 'text-white font-bold bg-blue-500' : 'text-gray-700 dark:text-gray-200' }}">
+                                        @if ($date['isToday']) bg-blue-500 text-white font-bold
+                                        @elseif ($date['isCurrentMonth']) bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-200
+                                        @else bg-gray-200 dark:bg-gray-600 text-gray-400 @endif">
                                     {{ $date['day'] }}
                                 </div>
                             @endforeach
