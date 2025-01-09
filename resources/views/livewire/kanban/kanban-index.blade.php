0ada0c2a89bd8a97b0e3abdabb8734982e196002
kytonas <faikarksatria@gmail.com>
2025-01-09 10:11:22 +0700

    fix: delete confirmation

--- resources/views/livewire/kanban/kanban-index.blade.php
+++ resources/views/livewire/kanban/kanban-index.blade.php
@@ -171,6 +171,32 @@
                                     title="Delete">
                                     <i class="bx bx-trash text-xl text-red-500"></i>
                                 </button>
+
+                                  <!-- Modal Konfirmasi -->
+                                <dialog id="my_modal_{{ $task->id }}"
+                                    class="modal fixed inset-0 flex items-center justify-center">
+                                    <div
+                                        class="modal-box bg-white text-gray-800 dark:bg-gray-800 dark:text-white p-4 md:p-5">
+                                        <svg class="mx-auto mb-4 text-gray-400 w-20 h-20 dark:text-gray-200"
+                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                        </svg>
+                                        <h3 class="text-lg font-bold">Apakah anda mau menghapus task ini?</h3>
+                                        <div class="modal-action">
+                                            <button
+                                                class="btn bg-red-500 text-white hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700 border-none"
+                                                wire:click="destroy({{ $task->id }})">
+                                                Hapus
+                                            </button>
+                                            <button class="btn hover:bg-gray-900 dark:bg-gray-700 dark:text-white"
+                                                onclick="my_modal_{{ $task->id }}.close()">
+                                                Batal
+                                            </button>
+                                        </div>
+                                    </div>
+                                </dialog>
                             </div>
                         </div>
                     @endforeach
